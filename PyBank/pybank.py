cf3f874bcdda63aa97449ce2b482883ea900e94c
Arvind Bhamidipati <arvindbhamidipati@users.noreply.github.com>
2020-09-07 12:02:45 -0700

    Added Greatest increase and decrease in code

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -27,9 +27,10 @@ with open(budget_data, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
     # skip header row
-    print("Header: ", str(csv_header))
-
+    #print("Header: ", str(csv_header))
     #The total number of months included in the dataset
+    print("Financial Analysis")
+    print("---------------------")
     months = []
     Total = []
     for rows in csvreader:
@@ -58,5 +59,17 @@ with open(budget_data, newline="") as csvfile:
     length_montly_change = len(monthly_change)
     sum_of_montly_change = sum(monthly_change)
     total_average = sum_of_montly_change / length_montly_change
+    formatted_total_average = "{:.2f}".format(total_average)
 
-    print("Average of changes in Profit/Losses: $", str(total_average))
+    print("Average of changes in Profit/Losses: $", str(formatted_total_average))
+
+    #The greatest increase in profits (date and amount) over the entire period
+    greatest_increase_rev = max(monthly_change)
+    greatest_decrease_rev = min(monthly_change)
+    greatest_increase_month = str(months[monthly_change.index(greatest_increase_rev)+1])
+    greatest_decrease_month = str(months[monthly_change.index(greatest_decrease_rev)+1])
+
+    print("Greatest increase in profits: ", greatest_increase_month, " $", str(greatest_increase_rev))
+    print("Greatest decrease in profits: ", greatest_decrease_month, " $", str(greatest_decrease_rev))
+
+    
